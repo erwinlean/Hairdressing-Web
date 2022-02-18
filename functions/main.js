f748d6a2b15b873ad8a1990062dadfbc10ea4b79
Erwin Marte <erwin.mdq@gmail.com>
2022-02-18 13:56:08 -0300

    Peluqueria v.0.1 - cambios anotados en el gitHub

--- functions/main.js
+++ functions/main.js
@@ -1,4 +1,4 @@
-const images = ["./img/img_Cortes/1.webp","./img/img_Cortes/11.jpg","./img/img_Cortes/2.jpg","./img/img_Cortes/6.jpg","./img/img_Cortes/4.jpg","./img/img_Cortes/5.jpg","./img/img_Cortes/7.jpg","./img/img_Cortes/9.jpg","./img/img_Cortes/10.jpg","./img/img_Cortes/12.jpg"]; 
+const images = ["./img/img_Cortes/0.jpg","./img/img_Cortes/1.jpg","./img/img_Cortes/2.jpg","./img/img_Cortes/3.jpg","./img/img_Cortes/4.jpg","./img/img_Cortes/5.jpg","./img/img_Cortes/6.jpg","./img/img_Cortes/7.jpg","./img/img_Cortes/8.jpg","./img/img_Cortes/9.jpg","./img/img_Cortes/10.jpg","./img/img_Cortes/11.jpg","./img/img_Cortes/12.jpeg","./img/img_Cortes/13.jpg"];  
 index = 0;
 const changeImagen = () =>  {
     document.getElementById("img1").src = images[index];
@@ -7,6 +7,6 @@ const changeImagen = () =>  {
     }else{
         index++;
     }
-    setTimeout(changeImagen, 2500);
+    setTimeout(changeImagen, 2150);
 }
 window.onload = changeImagen();
